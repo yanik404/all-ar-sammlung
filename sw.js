da5f322e770fe049b3543e96acb2acf2f34834a9
yanik404 <yanik404@users.noreply.github.com>
2026-09-23 19:48:56 +0200

    Refresh cache after card ordering update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='all-ar-v2';
+const CACHE='all-ar-v3';
 const CORE=['./','./index.html','./manifest.webmanifest','./data/cards.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
