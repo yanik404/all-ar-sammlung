d231e8aa697fa95e84db0560e9870f47f6d88b90
yanik404 <yanik404@users.noreply.github.com>
2026-09-23 19:30:12 +0200

    Refresh PWA cache on data updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='all-ar-v1';
+const CACHE='all-ar-v2';
 const CORE=['./','./index.html','./manifest.webmanifest','./data/cards.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
